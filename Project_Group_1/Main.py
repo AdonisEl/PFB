b7be9ae884a1a86d279d3fb6a2fdae1d2ab53ea9
Said2602 <123740706+Said2602@users.noreply.github.com>
2023-01-30 16:37:02 +0800

    Adding Comments for Main

--- Project_Group_1/Main.py
+++ Project_Group_1/Main.py
@@ -1,13 +1,19 @@
+# Importing modules that are required 
 import Cash_On_Hand, Overheads, Profit_Loss
 
+# Import path method from pathlib
 from pathlib import Path
+# Extending file path to with the use of '/'
 file_path = Path.cwd() / "Project_Group_1" / "summary_report.txt"
+# Creating the file "summary_report.txt"
+file_path.touch()
 
+# Calling the functions in respective modules using module.function(), then assigning it to a variable 
 Overheads_Report = Overheads.Overheads_Calculator()
 COH_Report = Cash_On_Hand.COH_Calculator()
 Profit_Report = Profit_Loss.Profit_Calculator()
 
-
+# Opening the created file and appending the data from the modules:Cash on Hand, Overheads and Profit Loss, into the created file
 with file_path.open(mode="a",encoding= "UTF-8") as file:
     file.write(f"{Overheads_Report}")
     for line in COH_Report:
@@ -19,10 +25,3 @@ with file_path.open(mode="a",encoding= "UTF-8") as file:
     
     
 
-
-# for line in Overheads_Report:
-#         print(f"{line}")
-# for line in COH_Report:
-#         print(f"{line}")
-# for line in Profit_Report:
-#         print(f"{line}")
